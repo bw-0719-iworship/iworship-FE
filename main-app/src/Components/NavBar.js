52afdbddb81c65819fad8ec168c37ace7ad065f9
Emily Lagattolla <elagattolla@gmail.com>
2019-07-30 08:58:32 -0500

    created nav skeleton

---
+++ main-app/src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+
+  return (
+    <div className="nav-bar">
+      <h1>iWorship</h1>
+      <header>
+        <a href="#">Home</a>
+        <a href="#">Search</a>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
