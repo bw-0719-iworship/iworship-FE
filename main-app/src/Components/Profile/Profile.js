22c99771dca7016914470842b7eb371ac8dbdb34
Emily Lagattolla <elagattolla@gmail.com>
2019-07-31 17:11:34 -0500

    styled profile component and nav

--- main-app/src/Components/Profile/Profile.js
+++ main-app/src/Components/Profile/Profile.js
@@ -5,42 +5,49 @@ function Profile() {
 
   const LayoutSideBar = styled.div`
     height: 600px;
-    width : 200px;
+    width : 250px;
     border: 2px solid black;
+    border-radius: 7px;
   `;
 
   const InfoUser = styled.div`
     height: 200px;
-    width: 200px;
-    border: 2px solid red;
-    display: flex;
-    /* align-item: center; */
+    width: 100%;
   `;
 
   const ImageUser = styled.img`
     height: 100px;
     width: 100px;
-    border: 2px solid black;
     margin: 10px auto;
   `;
 
+  const TextUser = styled.p`
+    font-size: 1.4rem;
+  `;
+
   const InfoSite = styled.div`
-    width: 200px;
-    padding: 10px;
-    border: 2px solid red;
+    width: 100%;
+    height: 400px;
+    background-image: url("https://i.imgur.com/uPb6WfY.jpg");
+    background-size: cover;
   `;
 
   const TextSite = styled.p`
-    font-size: 1.6rem;
+    font-size: 1.8rem;
+    padding: 15px;
+    line-height: 1.3;
+    color: white;
+    font-weight: bold;
   `;
 
   return (
     <LayoutSideBar>
       <InfoUser>
-        <ImageUser src="../assets/user.jpg" alt="Profile" />
+        <ImageUser src="https://image.flaticon.com/icons/png/512/17/17004.png" alt="Profile" />
+        <TextUser>Welcome, Gretta</TextUser>
       </InfoUser>
       <InfoSite>
-        <TextSite>Lorem Ipsum</TextSite>
+        <TextSite>Explore Faith</TextSite>
       </InfoSite>
     </LayoutSideBar>
   )
--- main-app/src/Components/map/Map.js
+++ main-app/src/Components/map/Map.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-
-
-
-const MapData = ReactMapboxGl({
-    accessToken: "pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"
-  });
-
-
-
-const Map = () => {
-
-    return(
-        <MapData   
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
-          height: "60vh",
-          width: "60vw"
-
-         
-        }}  center={[-77.0364, 38.8951]}>
-
-            <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}
-            >
-               {/* Marker display loop */}
-
-            <Feature coordinates={[38.2700, 89.8603]}/>
-
-            </Layer>
-
-
-        </MapData>
-    )
-}
-
-export default Map
+// import React from 'react';
+// import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+//
+//
+//
+// const MapData = ReactMapboxGl({
+//     accessToken: "pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"
+//   });
+//
+//
+//
+// const Map = () => {
+//
+//     return(
+//         <MapData
+//         style="mapbox://styles/mapbox/streets-v9"
+//         containerStyle={{
+//           height: "60vh",
+//           width: "60vw"
+//
+//
+//         }}  center={[-77.0364, 38.8951]}>
+//
+//             <Layer
+//             type="symbol"
+//             id="marker"
+//             layout={{ "icon-image": "marker-15" }}
+//             >
+//                {/* Marker display loop */}
+//
+//             <Feature coordinates={[38.2700, 89.8603]}/>
+//
+//             </Layer>
+//
+//
+//         </MapData>
+//     )
+// }
+//
+// export default Map
