8110a8ffb63c9ef1320cceb9307fa1e884468fa2
MadisonViaLux <madison.s.mckown@gmail.com>
2019-08-01 15:32:36 -0700

    HOLY MAP WORKING BATMAN

--- main-app/src/Components/Home.js
+++ main-app/src/Components/Home.js
@@ -1,38 +1,99 @@
+// import React, { useState, useEffect } from "react";
+// import axios from 'axios'
+// import '../index.scss'
+
+
+// import Cards from './grid/Cards'
+// import Map from './map/Map'
+// import Profile from "./Profile/Profile";
+// import NavBar from './NavBar/NavBar';
+
+// import { CardsContext } from './context/CardsContext'
+
+
+// // New data with lat and long 
+// // https://api.myjson.com/bins/1apvk1
+
+// // older data just basic n/e/g
+// // https://api.myjson.com/bins/7gt6p
+
+
+
+// function Home() {
+
+
+//   const [churches, setChurches] = useState([])
+
+
+//   useEffect( () => {
+//       axios.get('https://api.myjson.com/bins/1apvk1')
+//       .then(res => {
+//           console.log(res)
+//           setChurches(res.data)
+//       })
+//   }, [])
+
+
+
+//   return (
+//     <div className="home">
+
+//       <div className='homeComp'>
+//         <Profile />
+//       </div>
+
+//       <div className='homeComp'>
+
+//         <CardsContext.Provider value={churches} className='cardsHomeContainer'>
+
+//           <NavBar />
+
+//           <Map />
+
+//           <Cards />
+
+//         </CardsContext.Provider>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import '../index.scss'
-
-
 import Cards from './grid/Cards'
 import Map from './map/Map'
 import Profile from "./Profile/Profile";
 import NavBar from './NavBar/NavBar';
 
-import { CardsContext } from './context/CardsContext'
-
-
-// New data with lat and long 
-// https://api.myjson.com/bins/1apvk1
-
-// older data just basic n/e/g
-// https://api.myjson.com/bins/7gt6p
-
-
 
 function Home() {
 
+const [churches, setChurches] = useState([])
 
-  const [churches, setChurches] = useState([])
+const getCards = () => {
+  axios.get('https://api.myjson.com/bins/1apvk1')
+  .then(res => {
+      console.log(res)
+     setChurches(res.data)
+  })
 
-
+}
+ 
   useEffect( () => {
-      axios.get('https://api.myjson.com/bins/1apvk1')
-      .then(res => {
-          console.log(res)
-          setChurches(res.data)
-      })
+      getCards()
   }, [])
-
 
 
   return (
@@ -43,16 +104,14 @@ function Home() {
       </div>
 
       <div className='homeComp'>
-
-        <CardsContext.Provider value={churches} className='cardsHomeContainer'>
+      
 
           <NavBar />
 
-          <Map />
+           <Map churches={churches}/> 
 
-          <Cards />
+          <Cards  churches={churches} />
 
-        </CardsContext.Provider>
       </div>
     </div>
   );
--- main-app/src/Components/map/Map.js
+++ main-app/src/Components/map/Map.js
@@ -1,9 +1,13 @@
-import React from 'react';
+// import React from 'react';
+// import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone"
+// import ReactMapGL, { Marker } from "react-map-gl";
 
-import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+import React, { useState } from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl'
+import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone";
 
-import { CardsContext } from '../context/CardsContext'
-import { useContext } from 'react';
+// import { CardsContext } from '../context/CardsContext'
+// import { useContext } from 'react';
 
 // const coords= [{lat: 40.560001, long: -74.290001}]
 
@@ -23,156 +27,87 @@ import { useContext } from 'react';
 {/* <img src={require ('../../assets/drawing.svg')} alt='heart for churches' /> */}
 
 
-const MapData = ReactMapboxGl({
-    accessToken: "pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"
-  });
 
+// const Map = () => {
 
+//   let viewport = {
+//     width: 400,
+//     height: 400,
+//     latitude: 37.7577,
+//     longitude: -122.4376,
+//     zoom: 8
+//   }
 
-const Map = () => {
+//     const state = useContext(CardsContext)
+//     console.log(state)
 
-
-
-    const state = useContext(CardsContext)
-    console.log(state)
-
-    // function newState(){
-    //   {state.map(e => {
-    //     return {
-    //       lat: e.latitude,
-    //       long: e.longitude
-    //     }
-    //   })}
-    // }
-
-
-    function renderingMap(){
+//     function renderingMap(){
       
-      {state.map(e => {
+//       {state.map(e => {
 
-        console.log(e)
-        return(
-           <Marker key={e.id} latitude={e.latitude} longitude={e.longitude} >
-             <div>HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEllo</div>
-           </Marker>
+//         return(
+//            <Marker key={e.id} latitude={e.latitude} longitude={e.longitude} >
+//             <PlaceTwoTone/>
+//            </Marker>
 
-        )
-     })}
-    }
-
-
-    return(
-      <div>
-          <MapData
-          style="mapbox://styles/mapbox/streets-v9"
-          containerStyle={{
-            height: "60vh",
-            width: "60vw"         
-          }}  
-          center={[-77.0364, 38.8951]}>
-
-              {/* <Layer
-              type="symbol"
-              id="marker"
-              layout={{ "icon-image": "marker-15" }}
-              > */} */}
-
-                {renderingMap()}
-
-              {/* </Layer> */}
-          </MapData>
-      </div>
-    )
-}
-
-export default Map
+//         )
+//      })}
+//     }
 
 
+//     return(
+//       <div>
+//           <ReactMapGL
+//           width={700}
+//           height={450}
+//           latitude={37.78}
+//           longitude={-122.45}
+//           zoom={11}
+//           mapboxApiAccessToken={"pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"}
+//           mapStyle={'mapbox://styles/mapbox/basic-v9'}
+//        >
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-// import ReactMapGL, { Marker } from 'react-map-gl'
-
-// import { CardsContext } from '../context/CardsContext'
-// import { useContext } from 'react';
-
-
-
-// export default function Map() {
-//   const [viewport, setViewport] = useState({
-//     latitude: 45.4211,
-//     longitude: -75.6903,
-//     width: '60vw',
-//     height: '60vh'
-//   });
-
-//   const state = useContext(CardsContext)
-
-//   return(
-//     <div>
-
-//       <ReactMapGL 
-//       {...viewport} 
-//       mapboxApiAccessToken={"pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"}
-//       onViewportChange={viewport => {
-//         setViewport(viewport);
-//       }}
-//       >
-
-//         {state.map(churches => (
-
-//           console.log(churches.longitude),
-
-//           <Marker key={churches.id} coordinates={[churches.latitude, churches.longitude]}>
-//             <div>HELLO THERE</div>
-//           </Marker>
-//         ))}
-
-//       </ReactMapGL>
-
-
-//     </div>
-//   )
-
-
+//             {state.map((item, index)=> (
+//                <Marker key={index}  latitude={item.latitude} longitude={item.longitude} >
+//                <PlaceTwoTone/>
+//               </Marker>
+//             ))}
+//           </ReactMapGL>
+//       </div>
+//     )
 // }
 
+// export default Map
 
 
 
 
-// var map = new mapboxgl.Map({
-//   container: 'map', // HTML container id
-//   style: 'mapbox://styles/mapbox/streets-v9', // style URL
-//   center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-//   zoom: 13
-// });
 
-// var popup = new mapboxgl.Popup()
-// .setHTML('<h3>Reykjavik Roasters</h3><p>A good coffee shop</p>');
 
-// var marker = new mapboxgl.Marker()
-// .setLngLat([-21.9270884, 64.1436456])
-// .addTo(map);
+
+export default function Map({churches}) {
+  const [viewport, setViewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: 500,
+    height: 700,
+    zoom:8
+  });
+
+  return (
+    <ReactMapGL {...viewport}
+      mapboxApiAccessToken={"pk.eyJ1IjoibWFkaXNvbi1tY2tvd24iLCJhIjoiY2p5cTQ3NGJ5MW1kNDNtczhyd3Njdm1jbyJ9.nBC37rZnhwGXRe9TCfRnKg"}
+      onViewportChange={viewport => setViewport(viewport)}>
+        {churches.map((item, index) => (
+         <Marker latitude={Number(item.latitude)}
+                 longitude={Number(item.longitude)} 
+                 offsetLeft={-20} 
+                 offsetTop={-10}>
+          {/* <div style ={{fontSize: "30px"}}>You are here</div> */}
+          <PlaceTwoTone/>
+                    </Marker>
+                        ))}
+     
+    </ReactMapGL>
+  );
+}
